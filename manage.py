6d0a1ece19a41730ae19fe34bc3ba61baa851e75
r_jumaniyozov <r.jumaniyozov@uzinfocom.uz>
2025-10-17 23:04:47 +0500

    remove duble db

--- manage.py
+++ manage.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
+
 import os
 import sys
 
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraper_api_service.settings')
-    from django.core.management import execute_from_command_line
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Django import qilinmadi. Django o‘rnatilganini va virtual muhit to‘g‘ri yoqilganini tekshiring."
+        ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
